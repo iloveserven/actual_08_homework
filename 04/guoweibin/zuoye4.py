4927225e6fde604cf5b96aceb4efedb032cb704c
gwbin1988 <751629731@qq.com>
2016-03-28 20:20:40 +0800

    郭伟斌第四次作业

---
+++ 04/guoweibin/zuoye4.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import sys
+
+log_dict={}
+result=[]
+##打开文件，并将用户Ip，访问连接,访问状态作为keys，出现次数作为values，形成字典
+try:
+    with open('www_access_20140823.log') as f:
+          for line in f:
+               state_list=line.split()
+               log_dict.setdefault((state_list[0],state_list[6],state_list[8]),0)
+               log_dict[state_list[0],state_list[6],state_list[8]]+=1
+          # print log_dict
+except IOError as e:
+    print "The log file is not exist"
+    sys.exit(1)
+
+#将形成的结果字典反转
+new_dict={}
+for key,val in log_dict.items():
+    new_dict.setdefault(val,[])
+    new_dict[val].append(key)
+# print new_dict
+
+
+res_str='<table border="1" align="center">'
+tmp=''' <tr>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+        <td>%s</td>
+    </tr>'''
+res_str+=tmp%('排名','访问次数','用户IP','访问链接','访问HTTP状态')
+
+count=1
+key_var=new_dict.keys()
+str_list=[]
+
+while count<11:
+    max_count=max(key_var)
+    length=len(new_dict[max_count])
+
+    if length==1:
+      res_str+=tmp%(count,max_count,new_dict[max_count][0][0],new_dict[max_count][0][1],new_dict[max_count][0][2])
+    elif length>1:
+        for i in range(length):
+           res_str+=tmp%(count,max_count,new_dict[max_count][i][0],new_dict[max_count][i][1],new_dict[max_count][i][2])
+    key_var.remove(max_count)
+
+
+    count=count+len(new_dict[max_count])
+res_str+='</table>'
+f=open('zuoye4.html','w')
+f.write(res_str)
+f.close()
+
+# count=1
+# val_arr=log_dict.values()
+# print val_arr
+# str_list=[]
+# max_count=max(val_arr)
+# print max_count
+
+#     print '%s,字符是%s,出现了%s次'%(count,result_dict[max_count],max_count)
+#
+#     key_arr.remove(max_count)
+#     count=count+len(result_dict[max_count])
+
+
+
+
