138c159692fb89f60f8578fcabd9d328b7a9be1b
root <616225996@qq.com>
2016-04-11 15:28:28 +0800

    hello

--- 02/zhangwenbo/web3.15.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-arr = [3,1,4,5,10,2]
-for x in range(1, len(arr)):
-	for i in range(x,0,-1):
-		if arr[i] < arr[i - 1]:
-			arr[i], arr[i - 1] = arr[i - 1], arr[i]
-		else:
-			break		
-print arr
