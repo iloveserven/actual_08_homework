8b19153d1b1f46422f34ef989ebc0e76adfebecc
zhugaojian <zhugaojian@126.com>
2016-04-23 23:01:13 +0800

    homework update

--- 06/zhugaojian/homework.py
+++ 06/zhugaojian/homework.py
@@ -14,12 +14,13 @@ def index():
 def userlist():
     if 'user' not in session:
         return redirect('/login')
-    user_list = []
+    user_dict = {}
     with open('user.txt') as f:
         for i in f.readlines():
-            if len(i.strip().split(':')) == 2:
-                user_list.append(i.strip().split(':'))
-    return render_template('userlist.html',user_list=user_list)
+            tmp = i.strip().split(':')
+            if len(tmp) == 2:
+                user_dict[tmp[0]] = tmp[1]
+    return render_template('userlist.html',user_list=user_dict.items())
 
 # 添加用户，不验证用户名是否重复
 @app.route('/useradd',methods=['post','get'])
@@ -32,6 +33,14 @@ def user_added():
     # 用户名或者密码是否为空则不添加
     if not name or not password:
         return redirect('/userlist')
+    user_dict = {}
+    with open('user.txt') as f:
+        for i in f.readlines():
+            tmp = i.strip().split(':')
+            if len(tmp) == 2:
+                user_dict[tmp[0]] = tmp[1]
+    if name in user_dict:
+        return "user exist <a href='/userlist'>返回</a>"
     with open('user.txt','a') as f:
         f.writelines('\n'+name+':'+password)
     return redirect('/userlist')
@@ -44,20 +53,21 @@ def user_del():
     # 防止不选择导致出错
     if not request.form.get('name'):
         return redirect('/userlist')
-    name_list = request.form.get('name').split('_')
-    file_str = ''
-    user_list = []
+    name = request.form.get('name')
+    user_dict = {}
     with open('user.txt') as f:
         for line in f:
-            if len(line.strip().split(':')) == 2:
-                user_list.append(line.strip().split(':'))
-    user_list.remove(name_list)
-    print name_list
-    print user_list
-    for i in user_list:
-        file_str += '\n'+':'.join(i)
+            tmp = line.strip().split(':')
+            if len(tmp) == 2:
+                user_dict[tmp[0]] = tmp[1]
+    if not name in user_dict:
+        return "user not exist <a href='/'>返回</a>"
+    user_dict.pop(name)
+    user_list = []
+    for user,pwd in user_dict.items():
+        user_list.append(user+':'+pwd)
     with open('user.txt','w') as f:
-        f.writelines(file_str[1:])
+        f.writelines('\n'.join(user_list))
     return redirect('/userlist')
 
 # 登录页
@@ -72,7 +82,6 @@ def login():
 def loginaction():
     name = request.form.get('name')
     password = request.form.get('password')
-    print 'name:'+name+'  password:'+password
     if name=='admin' and password=='admin':
         session['user']=name
     return redirect('/userlist')
@@ -82,6 +91,38 @@ def loginaction():
 def logout():
     session.pop('user')
     return redirect('/login')
+
+@app.route('/useredit',methods=['get','post'])
+def useredit():
+    if 'user' not in session:
+        return redirect('/login')
+    name = request.args.get('user')
+    return render_template('useredit.html',name=name)
+
+@app.route('/usereditaction',methods=['get','post'])
+def usereditaction():
+    if 'user' not in session:
+        return redirect('/login')
+    # 防止不选择导致出错
+    if not request.form.get('name'):
+        return redirect('/userlist')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    user_dict = {}
+    with open('user.txt') as f:
+        for line in f:
+            tmp = line.strip().split(':')
+            if len(tmp) == 2:
+                user_dict[tmp[0]] = tmp[1]
+    if not name in user_dict:
+        return "user not exist <a href='/'>返回</a>"
+    user_dict[name] = password
+    user_list = []
+    for user,pwd in user_dict.items():
+        user_list.append(user+':'+pwd)
+    with open('user.txt','w') as f:
+        f.writelines('\n'.join(user_list))
+    return redirect('/userlist')
 
 
 if __name__=='__main__':
