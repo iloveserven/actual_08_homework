735261f3797feabc656e30b3e4b0ab0230d894ab
xiongtao <296344731@qq.com>
2016-04-24 15:40:51 +0800

    Update zouye.py

--- 06/xiongtao/zouye.py
+++ 06/xiongtao/zouye.py
@@ -1,36 +1,15 @@
-
-#coding:utf-8
-#redirect 
 from flask import Flask,render_template,request,redirect,session
-
+import aadat
 app = Flask(__name__)
+aadat.update_data()
 app.secret_key='dnaindi2398r2idnao01nidsnoi12ni'
-
-@app.route('/')
-def index():
-       username = 'admin'
-       return render_template('index011.html',username=username)
-
-@app.route('/lixiaolong')
-def lixiaolong():
-       if 'user' not in session:
-                return redirect('/login')
-       userlist = []
-       with open('./userss.txt') as f:
-                for line in f:
-                      tmp = line.split(':')
-                      if  len(tmp) == 2:
-                            userlist.append(tmp)
-       return render_template('lixiaolong.html',userlist=userlist)
 
 @app.route('/login')
 def login():
-        return render_template('login.html')
-
-@app.route('/logout')
-def logout():
-        session.pop('user')
-        return redirect('/login')
+        if 'user' in session:
+                return redirect('/')
+        else:
+                return render_template('login.html')
 
 @app.route('/loginaction',methods=['post'])
 def loginaction():
@@ -38,34 +17,41 @@ def loginaction():
         passwd = request.form.get('passwd')
         if user=='admin'and passwd=='admin':
                 session['user'] = 'admin'
-                return redirect('/lixiaolong')
+                return redirect('/')
+        else:
+                return 'not alloweb'
 
+@app.route('/logout')
+def logout():
+        session.pop('user')
+        return redirect('/login')
+
+@app.route('/')
+def index():
+        if 'user' not in session:
+                return redirect('/login')
+        return render_template('data.html',userlist=aadat.file_data.items())
 @app.route('/adduser',methods=['post'])
 def adduser():
-        print request.args
         user = request.form.get('user')
         password = request.form.get('password')
-        with open('./userss.txt','a') as f:
-                f.write('\n'+user+':'+password)
-        return redirect('/lixiaolong')
+        print 'user is %s and passw is %s'%(user,password)
+        if user in aadat.file_data:
+                return 'user alrredy eists'
+        else:
+                aadat.file_data[user]=password
+                aadat.update_file()
+                return redirect('/')
 
-@app.route('/Duser',methods=['post'])
+@app.route('/Deuser')
 def Deuser():
-    user = request.form.get('user')
-    if user:
-          with open('./userss.txt','r') as f:
-                tmplist=f.readlines()
-          with open('./userss.txt','w') as file_:
-                for line in tmplist:
-                        line_ = line.split()
-                        user_ = line.split()
-                        if line_ == user_:
-                            continue
-                        else:
-                                file_.write(line)
-          return redirect('/lixiaolong')
-    return "deletenum"
-
+        print request.args
+        user=request.args.get('user')
+        if user in aadat.file_data:
+                aadat.file_data.pop(user)
+                print aadat.file_data
+                aadat.update_file()
+        return redirect('/')
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0',port=9999,debug=True)
