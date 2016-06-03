d9e2bb3f88184ca7eaa89b3c8836bd3274e4220f
xtyygyihc <yygyihc182@sina.com>
2016-06-03 22:32:13 +0800

    zuoye

--- 10/zhangwenbo/dbutil.py
+++ 10/zhangwenbo/dbutil.py
@@ -5,7 +5,7 @@ cursor = None
 def connect():
 	global con
 	global cursor
-	con = mysql.connect(db='web',host='localhost',user='root',passwd='mysql')
+	con = mysql.connect(db='web',charset='utf8',host='localhost',user='root',passwd='mysql')
 	con.autocommit(True)
 	cursor = con.cursor()
 
